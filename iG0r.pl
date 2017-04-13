137de02cd1e598cd959aaec645b4dffc21592fc2
davehhpnet <dave@thehhp.net>
2017-04-12 22:39:05 -0500

    Changed the bot to aggregate values out of ~/.igorrc
    - NICK
    - SERVER
    - PORT
    - USESSL
    - CHANNELS

--- iG0r.pl
+++ iG0r.pl
@@ -8,23 +8,41 @@ use URI::Title qw / title /;
 use strict;
 use warnings;
 
-POE::Session->create(
-    package_states => [ main => [qw/ _default _start irc_join /] ] );
+use Data::Dumper;    ## DEBUG
+
+my $conf;
+
+## Setup configuration values from .igorrc
+my $igorrc = '.igorrc';
+open my $rc_f, $igorrc or die "Unable to open $igorrc.";
+while ( my $line = <$rc_f> ) {
+    next if $line =~ /^#|^$/;
+    chomp $line;
+    if ( $line =~ /(.*)=(.*)/ ) {
+        $conf->{$1} = $2;
+    }
+}
+my $array = [];
+@$array = split( /,/, $conf->{'CHANNELS'} );
+$conf->{'CHANNELS'} = $array;
+print Dumper $conf;
+
+POE::Session->create( package_states => [ main => [qw/ _default _start irc_join /] ] );
 
 $poe_kernel->run();
 
 sub _start {
     my $irc = POE::Component::IRC::State->spawn(
-        Nick   => 'iG0r',
-        Server => 'teodesian.net',
-        Port => '6697',
-        UseSSL => 'true',
+        Nick   => $conf->{'NICK'},
+        Server => $conf->{'SERVER'},
+        Port   => $conf->{'PORT'},
+        UseSSL => $conf->{'USESSL'},
     );
 
     $irc->plugin_add(
         'AutoJoin',
         POE::Component::IRC::Plugin::AutoJoin->new(
-             Channels => [ '#liberty', ]
+             Channels => $conf->{'CHANNELS'}
         )
     );
 
@@ -47,10 +65,10 @@ sub irc_join {
 sub _default {
     my ( $event, $args ) = @_[ ARG0 .. $#_ ];
     my $channel = $_[ARG1][1];
-    my $user = (split /!/, $_[ARG1][0])[0];
+    my $user    = ( split /!/, $_[ARG1][0] )[0];
     my $message = $_[ARG1][2];
-    my @output = ("$event: ");
-    my $irc = $_[SENDER]->get_heap();
+    my @output  = ("$event: ");
+    my $irc     = $_[SENDER]->get_heap();
 
     for my $arg ($args) {
         if ( ref $arg eq 'ARRAY' ) {
@@ -63,22 +81,46 @@ sub _default {
     print join ' ', @output, "\n";
 
     use Data::Dumper;
-    print Dumper($_[ARG1][2]);
+    print Dumper( $_[ARG1][2] );
 
-    if ( $event eq 'irc_public' && $user =~/y000da*/ && $message eq 'you stink' ) {
-        $irc->yield( privmsg => $channel, 'Sorry about that.  I had Mexican for lunch heh, ' . $user );
+    if (   $event eq 'irc_public'
+        && $user =~ /y000da*/
+        && $message eq 'you stink' )
+    {
+        $irc->yield(
+            privmsg => $channel,
+            'Sorry about that.  I had Mexican for lunch heh, ' . $user
+        );
     }
-    if ( $event eq 'irc_public' && $message =~ /(^|\s+)(black|brown|yellow|red|green|blue|purple|gray|indigo|chartrues|orange|liberal(s*))(\s+|$)/i ) {
-        $irc->yield( privmsg => $channel, 'That\'s racist, ' . $user . '. http://zen.thehhp.net/albums/funny/thats_racist_wtf.gif' );
+    if (   $event eq 'irc_public'
+        && $message =~
+/(^|\s+)(black|brown|yellow|red|green|blue|purple|gray|indigo|chartrues|orange|liberal(s*))(\s+|$)/i
+      )
+    {
+        $irc->yield(
+            privmsg => $channel,
+            'That\'s racist, '
+              . $user
+              . '. http://zen.thehhp.net/albums/funny/thats_racist_wtf.gif'
+        );
     }
-    if ( $event eq 'irc_public' && $message =~ /(^|\s+)(build|rebuilt|built)(\s+|$)/i ) {
-        $irc->yield( privmsg => $channel, 'Pfft, ' . $user . '. http://zen.thehhp.net/albums/liberty/OBAMA-you_didnt_build_that.jpg' );
+    if (   $event eq 'irc_public'
+        && $message =~ /(^|\s+)(build|rebuilt|built)(\s+|$)/i )
+    {
+        $irc->yield(
+            privmsg => $channel,
+            'Pfft, '
+              . $user
+              . '. http://zen.thehhp.net/albums/liberty/OBAMA-you_didnt_build_that.jpg'
+        );
     }
     if ( $event eq 'irc_public' && $message =~ /(https?:\/\/[www.]?\w.*)/i ) {
         print "DDDD REGEX: $1\n";
         my $title = title($1);
+
+        #my $title = WWW::Mechanize::URI::Title::get_title($1);
         print "TITLE-->$title\n" if defined $title;
-        $irc->yield( privmsg => $channel, "<-[ $title ]->" ) if defined $title;;
+        $irc->yield( privmsg => $channel, "<-[ $title ]->" ) if defined $title;
     }
     return 0;
 }
